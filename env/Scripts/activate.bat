86b57325fd59a56d8016e86e1292223e0f48bcac
bberthel <brice.berthelot@sfr.fr>
2015-01-29 02:21:34 +0100

    Design update

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -1,5 +1,5 @@
 @echo off
-set "VIRTUAL_ENV=C:\Users\Florent\Desktop\imac3\web\diabon\env"
+set "VIRTUAL_ENV=C:\Users\Brice\Documents\GitHub\Diabeto\env"
 
 if defined _OLD_VIRTUAL_PROMPT (
     set "PROMPT=%_OLD_VIRTUAL_PROMPT%"
