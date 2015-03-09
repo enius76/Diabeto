8aff4858e1b5499add87cc1619f5fcc51121c806
Florent <enius76@hotmail.com>
2015-03-09 20:24:48 +0100

    ca bug moins mais c'est toujours nul

--- diabon/core/views.py
+++ diabon/core/views.py
@@ -8,7 +8,7 @@ from django.contrib.auth import authenticate, login
 from django.contrib.auth import logout
 from django.shortcuts import render
 from django.core.urlresolvers import reverse
-
+from core.forms import *
 
 
 def home(request):
