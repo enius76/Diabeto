942a265fbf5f73fbb4c51221b17eaa1aaf5700c5
bberthel <brice.berthelot@sfr.fr>
2015-03-21 22:07:17 +0100

    Some updates (design, inscription, etc.)

--- diabon/core/views.py
+++ diabon/core/views.py
@@ -146,23 +146,24 @@ def article_details(request, slug):
 # ________________________________________________ CONTACT ______________________________________________
 
 def contact(request):
-	return render_to_response('contact.html')
-
-def contact(request):
 	if request.method == "POST":
 		form = ContactForm(request.POST)
 		if form.is_valid():
 			subject = form.cleaned_data['subject']
 			message = form.cleaned_data['message']
 			sender = form.cleaned_data['sender']
-			cc_myself = form.cleaned_data['cc_myself']
-
 			recipients = ['briceberthelot64@gmail.com']
-			if cc_myself:
-				recipients.append(sender)
-
 			send_mail(subject, message, sender, recipients)
 			return HttpResponseRedirect('/contact-effectue/')
+	else:
+		form = ContactForm()
+	variables = RequestContext(request, {
+	'form': form
+	})
+	return render_to_response(
+	'contact.html',
+	variables,
+	)	
 
 def contactEffectue(request):
 	return render_to_response('merci.html')
